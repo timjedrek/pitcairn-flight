f1b0001b1486b79b56cd7602baeab13f1c7582c2
Tim Jedrek <tim@timjedrek.com>
2023-09-12 11:48:20 -0500

    add marisa email to mailer

--- app/mailers/message_confirmation_mailer.rb
+++ app/mailers/message_confirmation_mailer.rb
@@ -5,7 +5,7 @@ class MessageConfirmationMailer < ApplicationMailer
   def message_confirmation_email(message)
     @message = message
 
-    mail(to: @message.email, bcc: ["no-reply@pitcairnmailer.com"], subject: "✈️ Thank you for contacting Pitcairn Flight")
+    mail(to: @message.email, bcc: ["no-reply@pitcairnmailer.com", "marisa@pitcairnflight.com"], subject: "✈️ Thank you for contacting Pitcairn Flight")
   end
 
 end

4996ffa9ae7cbb2579b0fa9517722477f38c81f1
Tim Jedrek <tim@timjedrek.com>
2023-11-03 17:55:44 -0500

    change name of function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :initialize_multi_page_forms
+  before_action :initialize_contact_form
+  invisible_captcha only: [:initalize_contact_form], honeypot: :confirm_email
 
   private
 
-  def initialize_multi_page_forms
+  def initialize_contact_form
     #@enrollment = Enrollment.new
     #@book_download = BookDownload.new
     @message = Message.new

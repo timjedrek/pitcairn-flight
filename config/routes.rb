a192ffb8e4fc2aabb64d718f6402a5a4497c9a8e
Raul Ospina <raminka13@hotmail.com>
2024-03-05 09:10:23 -0500

    Update: url path to recreational-flights

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get 'errors/internal_server_error'
     match "/404", to: "errors#not_found", via: :all
     match "/500", to: "errors#internal_server_error", via: :all
-  
+
   devise_scope :admins do
     # Redirests signing out users back to sign-in
     get "admins", to: "devise/sessions#new"
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
   root "pages#home"
   #root "messages#home2"
 
-  post 'uploader/image', to: 'uploader#image' #add upload image to posts 
+  post 'uploader/image', to: 'uploader#image' #add upload image to posts
   get 'blog', to: 'posts#index', as: :blog
   resources :posts
 
-  resources :recreational_flights, path: 'recreational-flights'
+  resources :recreational_flights, path: 'scenic-rides'
   resources :discover_flights
   resources :messages
   resources :enrollments
@@ -76,9 +76,9 @@ Rails.application.routes.draw do
   #get 'meritize', to: 'pages#meritize', as: :meritize
   #get 'accelerated-pilot-training', to: 'pages#accelerated', as: :accelerated
   #get 'flexible-pilot-training', to: 'pages#flexible', as: :flexible
-  
+
   #get 'financing', to: 'pages#financing', as: :financing
-  #get 'pilot-training-pricing-calculator', to: 'pages#cost' 
+  #get 'pilot-training-pricing-calculator', to: 'pages#cost'
   #get 'approximate-pilot-training-costs', to: 'pages#cost', as: :cost
   #get 'timeline-to-be-a-pilot', to: 'pages#timeline', as: :timeline
   #get 'multi-engine-rating', to: 'pages#multi', as: :multi
@@ -86,17 +86,17 @@ Rails.application.routes.draw do
   #get 'airline-pilot-courses', to: 'pages#airline_pilot_courses', as: :airline_pilot_courses
   #get 'pilot-programs', to: 'pages#pilot_programs', as: :programs
   #get 'our-team', to: 'pages#our_team', as: :our_team
-  
+
   #get 'aircraft-rental', to: 'pages#aircraft_rental', as: :aircraft_rental
   #get 'fixed-wing-airplane-fleet', to: 'pages#fixed_wing', as: :fixed_wing
   #get 'rotary-wing-helicopter-fleet', to: 'pages#rotary_wing', as: :rotary_wing
   #get 'airplane-pilot-training', to: 'pages#airplane_training', as: :airplane_training
   #get 'helicopter-pilot-training', to: 'pages#helicopter_training', as: :helicopter_training
-  
+
   #get 'advanced_training', to: 'pages#advanced_training', as: :advanced_training
   #get 'pilot-resources', to: 'pages#pilot_resources', as: :pilot_resources
   #get 'careers', to: 'pages#careers', as: :careers
-  
+
 
   # Previous Site's Routes
   #get 'home/flight-training', to: 'pages#why_simplifly'
